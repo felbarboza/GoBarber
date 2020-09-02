9ed30690aee1120f8a9d061bd94af81215b7015b
Felipe Alves Barboza <50283187+felipeab03@users.noreply.github.com>
2020-09-01 22:05:56 -0300

    Filtrando Agendamentos por mês

--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,7 +1,8 @@
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentRepository implements IAppointmentsRepository {
@@ -26,6 +27,21 @@ class AppointmentRepository implements IAppointmentsRepository {
 		this.appointments.push(appointment);
 
 		return appointment;
+	}
+
+	public async findAllInMonthFromProvider({
+		provider_id,
+		month,
+		year,
+	}: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+		const appointments = this.appointments.filter(
+			appointment =>
+				appointment.provider_id === provider_id &&
+				getMonth(appointment.date) + 1 === month &&
+				getYear(appointment.date) === year,
+		);
+
+		return appointments;
 	}
 }
 
