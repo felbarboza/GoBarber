f7c0e25eb2b9c80720fc62e1a69bcd1ee69ef220
Felipe Alves Barboza <50283187+felipeab03@users.noreply.github.com>
2020-10-03 13:19:25 -0300

    feat(mobile): created new pages structure and routes

--- backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
 	public async index(
@@ -15,6 +16,6 @@ export default class ProvidersController {
 			user_id,
 		});
 
-		return response.json(providers);
+		return response.json(classToClass(providers));
 	}
 }
